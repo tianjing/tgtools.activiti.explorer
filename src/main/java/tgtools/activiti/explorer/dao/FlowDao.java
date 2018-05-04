0b3d3f0b42736b4fb6a863829edb395fdeb4c351
田径 <tian___jing@hotmail.com>
2018-05-04 20:47:11 +0800

    修正实体类字段映射问题

--- src/main/java/tgtools/activiti/explorer/dao/FlowDao.java
+++ src/main/java/tgtools/activiti/explorer/dao/FlowDao.java
@@ -1,5 +1,8 @@
 package tgtools.activiti.explorer.dao;
 
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.SelectProvider;
 import tgtools.activiti.explorer.entity.FlowLogVo;
 
@@ -14,5 +17,10 @@ import java.util.List;
 public interface FlowDao {
 
     @SelectProvider(type=FlowProvider.class,method = "getFlowLog")
+    @Results({ @Result(property = "mName", column = "NAME_") ,
+            @Result(property = "mLast", column = "LAST_"),
+            @Result(property = "mDescription", column = "DESCRIPTION_") ,
+            @Result(property = "mStartTime", column = "START_TIME_"),
+            @Result(property = "mEndTime", column = "END_TIME_")})
     List<FlowLogVo> getFlowLog(String pProcInstId);
 }
